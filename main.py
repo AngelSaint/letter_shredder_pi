340f58c582e3ae3557f06e115422d2528f606e84
Angelo <angelomsantos2412@gmail.com>
2023-04-10 15:31:37 -0500

    Testing MP

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
 import sys
+import multiprocessing as mp
+import time
 
-print("testing project", sys.argv[1])
+
+def func_1(time_begin, seconds):
+    time.sleep(seconds)
+    print("func_1", time.time()-time_begin)
+
+
+def func_2(time_begin, seconds):
+    time.sleep(seconds)
+    print("func_2", time.time()-time_begin)
+
+
+if __name__ == '__main__':
+    time_begin = time.time()
+    p1 = mp.Process(target=func_1, args=(time_begin, 3))
+    p2 = mp.Process(target=func_2, args=(time_begin, 2))
+    print("main", time.time()-time_begin)
 
