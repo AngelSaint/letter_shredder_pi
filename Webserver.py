53fa1554c5c91a5b06da303a8df5e06a729cea32
Angelo <angelomsantos2412@gmail.com>
2023-04-17 02:16:53 -0500

    Just a search page

--- Webserver.py
+++ Webserver.py
@@ -1,45 +1,53 @@
 from flask import Flask, render_template, request
 import mysql.connector
 
-# Create a Flask app
 app = Flask(__name__)
 
-# Establish a connection to the MariaDB SQL database
-conn = mysql.connector.connect(
-    user="angelos4",
-    password="12345678",
-    host="localhost",
-    database="letter_shredder"
+# Define MySQL database connection details
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="yourusername",
+  password="yourpassword",
+  database="yourdatabase"
 )
 
+# Define a function to execute SQL queries and return results
+def execute_query(query):
+  cursor = mydb.cursor()
+  cursor.execute(query)
+  result = cursor.fetchall()
+  return result
 
-# Define the function for the index page
-@app.route("/")
-def index():
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM Residents")
-    rows = cur.fetchall()
-    return render_template("index.html", rows=rows)
+# Define a route to display the search form
+@app.route('/')
+def search_form():
+    return render_template('search.html')
 
-
-# Define the function for the home page
-@app.route("/home")
-def home():
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM BlackList")
-    rows = cur.fetchall()
-    return render_template("home.html", rows=rows)
-
-
-# Define the function for the search page
-@app.route("/search")
+# Define a route to handle search requests
+@app.route('/search', methods=['GET', 'POST'])
 def search():
-    query = request.args.get("q")
-    cur = conn.cursor()
-    cur.execute(f"SELECT * FROM BlackList WHERE Name LIKE '%{query}%'")
-    rows = cur.fetchall()
-    return render_template("search.html", rows=rows)
+    if request.method == 'POST':
+        search_text = request.form['search_text']
+        search_results = []
 
+        # Execute SQL queries to search for the search_text in each table
+        query1 = "SELECT * FROM Residents WHERE Name LIKE '%" + search_text + "%'"
+        query2 = "SELECT * FROM BlackList WHERE Name LIKE '%" + search_text + "%'"
+        result1 = execute_query(query1)
+        result2 = execute_query(query2)
 
-if __name__ == "__main__":
-    app.run()
+        # Add the search results to the search_results list
+        if result1:
+            search_results.append(('Residents', result1))
+        if result2:
+            search_results.append(('BlackList', result2))
+
+        # Render the search results template
+        return render_template('search_results.html', search_text=search_text, search_results=search_results)
+
+    # If the request method is GET, display the search form
+    else:
+        return render_template('search.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
